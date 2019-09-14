c175ca8f253bb5e554004c92495eb7c9b6b216bb
davidteather <davidteather@users.noreply.github.com>
2019-09-14 12:28:29 -0500

    Added long description

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from distutils.core import setup
+import os.path
+
+try:
+    here = os.path.abspath(os.path.dirname(__file__))
+    README = open(os.path.join(here, "README.md"), encoding="utf-8").read()
+    with open(os.path.join(here, "requirements/base.txt"), encoding="utf-8") as f:
+        required = [l.strip("\n") for l in f if l.strip("\n") and not l.startswith("#")]
+except IOError:
+    required = []
+    README = ""
+
 setup(
   name = 'TikTokApi',         
   packages = ['TikTokApi'],   
-  version = '2.1.3',      
+  version = '2.1.4',      
   license='MIT',        
   description = 'The Unoffical TikTok API Wrapper in Python 3.',   
   author = 'David Teather',                   
   author_email = 'contact.davidteather@gmail.com',     
-  url = 'https://github.com/davidteather/tiktok-api',  
-  download_url = 'https://github.com/davidteather/TikTok-Api/archive/V2.1.3.tar.gz', 
+  url = 'https://github.com/davidteather/tiktok-api',
+  long_description=README,
+  long_description_content_type="text/markdown",  
+  download_url = 'https://github.com/davidteather/TikTok-Api/tarball/master', 
   keywords = ['tiktok', 'python3', 'api', 'unofficial', 'tiktok-api'], 
   install_requires=[
           'requests',
