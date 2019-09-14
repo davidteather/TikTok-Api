e1cdf5f5e51fbcd20823645a35dcd442043cb9db
davidteather <davidteather@users.noreply.github.com>
2019-09-13 22:13:35 -0500

    Updated tests

--- tests/test_trending.py
+++ tests/test_trending.py
@@ -14,3 +14,8 @@ def test_trending():
     assert getTrending(5) == 5
     assert getTrending(10) == 10
     assert getTrending(20) == 20
+
+
+def test_extended_trending():
+    assert getTrending(50) == 50
+    assert getTrending(100) == 100
