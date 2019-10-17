42e2fd645dd4a500bdd319276e01e16582562757
David Teather <34144122+davidteather@users.noreply.github.com>
2019-10-17 10:45:38 -0500

    Update tiktok.py

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -226,22 +226,24 @@ class TikTokapi:
                 for tiktok in data["body"]["itemListData"]:
                     response.append(tiktok)
                 while True:
-                    print(data)
-                    if count > len(response) and str(data['body']['hasMore']) == "True":
-                        url = "https://m.tiktok.com/share/item/list?id=&type=5&count=" + \
-                            str(count - len(response)) + "&minCursor=0&maxCursor=" + \
-                            data['body']['maxCursor'] + \
-                            "&_signature=" + self.signature
+                    try:
+                        if count > len(response) and str(data['body']['hasMore']) == "True":
+                            url = "https://m.tiktok.com/share/item/list?id=&type=5&count=" + \
+                                str(count - len(response)) + "&minCursor=0&maxCursor=" + \
+                                data['body']['maxCursor'] + \
+                                "&_signature=" + self.signature
 
-                        r = requests.get(url, headers={"authority": "m.tiktok.com", "method": "GET", "path": url.split("https://m.tiktok.com")[0], "scheme": "https", "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-                                                       "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.9", "cache-control": "max-age=0", "upgrade-insecure-requests": "1",
-                                                       "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
-                        data = r.json()
-                        if data["statusCode"] == 0:
-                            for tiktok in data["body"]["itemListData"]:
-                                response.append(tiktok)
-                    else:
-                        return response
+                            r = requests.get(url, headers={"authority": "m.tiktok.com", "method": "GET", "path": url.split("https://m.tiktok.com")[0], "scheme": "https", "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+                                                           "accept-encoding": "gzip, deflate, br", "accept-language": "en-US,en;q=0.9", "cache-control": "max-age=0", "upgrade-insecure-requests": "1",
+                                                           "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"})
+                            data = r.json()
+                            if data["statusCode"] == 0:
+                                for tiktok in data["body"]["itemListData"]:
+                                    response.append(tiktok)
+                        else:
+                            return response
+                    except:
+                        continue
 
 
     #
