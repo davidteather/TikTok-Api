b52c8fea8cef262eb26326e9249bffcdb0cc4c61
David Teather <34144122+davidteather@users.noreply.github.com>
2019-10-17 10:33:25 -0500

    Update TikTok.py for logging

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -226,6 +226,7 @@ class TikTokapi:
                 for tiktok in data["body"]["itemListData"]:
                     response.append(tiktok)
                 while True:
+                    print(data)
                     if count > len(response) and str(data['body']['hasMore']) == "True":
                         url = "https://m.tiktok.com/share/item/list?id=&type=5&count=" + \
                             str(count - len(response)) + "&minCursor=0&maxCursor=" + \
