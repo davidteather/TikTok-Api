d25fdaedf21dcea59cdba3a96d368bac7a639e46
davidteather <davidteather@users.noreply.github.com>
2019-12-20 11:47:07 -0600

    Updated Test

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,7 @@
 from TikTokApi import TikTokapi
 
 def getUser(results):
-    api = TikTokapi("browsermob-proxy/bin/browsermob-proxy", headless=False)
+    api = TikTokapi("browsermob-proxy/bin/browsermob-proxy", headless=True)
     result = api.userPosts("5058536", "MS4wLjABAAAAoRsCq3Yj6BtSKBCQ4rf3WQYxIaxe5VetwJfYzW_U5K8", count=results)
     api.quit_browser()
 
