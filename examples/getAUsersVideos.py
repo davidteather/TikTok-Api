07b957206b386df3b26bb373863dda453ae2885c
David Teather <34144122+davidteather@users.noreply.github.com>
2019-12-21 11:17:45 -0600

    Updated for new change

--- examples/getAUsersVideos.py
+++ examples/getAUsersVideos.py
@@ -7,12 +7,16 @@ api = TikTokapi("browsermob-proxy/bin/browsermob-proxy")
 results = 10
 
 # The TikTok user's ID, can be found in the JSON from trending
-id = "7119601"
+id = "6667259165620912134"
 
-trending = api.userPosts(id, count=results)
+# The TikTok user's secUid, can be found in the Json from trending
+secUid = "MS4wLjABAAAAmROcF0HiS0_I562i4obaZJDpFVZ5IegSTj_Z3zE-Mf_BDx0OoiibJ3TMeYeCLrhx"
+
+trending = api.userPosts(id, secUid, count=results)
 
 for tiktok in trending:
     # Prints the text of the tiktok
     print(tiktok['itemInfos']['text'])
 
 print(len(trending))
+api.quit_browser()
