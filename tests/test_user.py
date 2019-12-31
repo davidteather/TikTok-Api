626a94a3c60ff7ac75f3251f16112b84cbfe0544
davidteather <davidteather@users.noreply.github.com>
2019-12-31 12:53:59 -0600

    V2.3.5 - Removed Tests that don't work on travis

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,12 +8,3 @@ def test_user():
     assert len(api.userPosts(id="5058536", secUid="MS4wLjABAAAAoRsCq3Yj6BtSKBCQ4rf3WQYxIaxe5VetwJfYzW_U5K8", count=30)) == 30
 
     api.quit_browser()
-
-def test_username():
-    api = TikTokapi("browsermob-proxy/bin/browsermob-proxy", headless=True)
-
-    assert len(api.userPosts(username="thecardguy", count=5)) == 5
-    assert len(api.userPosts(username="thecardguy", count=10)) == 10
-    assert len(api.userPosts(username="thecardguy", count=27)) == 27
-
-    api.quit_browser()
