1180a6fcd6b6e3de455878b62f5460ba0ab7dcb7
davidteather <davidteather@users.noreply.github.com>
2020-05-13 14:39:36 -0500

    V3.1.0 - weird travis issue

--- tests/test_VideoByUrl.py
+++ tests/test_VideoByUrl.py
@@ -3,4 +3,4 @@ from TikTokApi import TikTokApi
 def test_get_video_by_url():
     api = TikTokApi()
     bytesV = api.get_Video_By_TikTok(api.trending(count=30)[0])
-    assert type(bytesV) == type(bytes())
+    assert type(bytesV) == type(bytes()) 
