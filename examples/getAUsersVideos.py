eaed2e7651627cb09cab05c845b42337a5077aba
Siddhartha Singh <58968275+sid2660@users.noreply.github.com>
2020-05-23 20:36:56 +0530

    Update getAUsersVideos.py

--- examples/getAUsersVideos.py
+++ examples/getAUsersVideos.py
@@ -1,13 +1,13 @@
 from TikTokApi import TikTokApi
-# Starts TikTokApi
+# Starts TikTokApi.
 api = TikTokApi()
 
-# The Number of trending TikToks you want to be displayed
+# The Number of trending TikToks you want to be displayed.
 results = 10
 
 # Returns a list of dictionaries of the trending object
 userPosts = api.userPosts("6745191554350760966", "MS4wLjABAAAAM3R2BtjzVT-uAtstkl2iugMzC6AtnpkojJbjiOdDDrdsTiTR75-8lyWJCY5VvDrZ", 30)
-# Loops over every tiktok
+# Loops over every tiktok.
 for tiktok in userPosts:
     # Prints the text of the tiktok
     print(tiktok['desc'])
