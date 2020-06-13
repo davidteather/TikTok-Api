4afeda6045edbcb1e718ff60f634edc35a11b89f
davidteather <davidteather@users.noreply.github.com>
2020-06-13 10:42:58 -0500

    Fixed Tests

--- tests/test_suggested.py
+++ tests/test_suggested.py
@@ -2,12 +2,12 @@ from TikTokApi import TikTokApi
 
 def test_suggested():
     api = TikTokApi()
-    assert len(api.getSuggestedHashtagsbyID()) > 0
-    assert len(api.getSuggestedMusicbyID()) > 0
+    assert len(api.getSuggestedHashtagsbyID()) == 30
+    assert len(api.getSuggestedMusicbyID()) == 30
     assert len(api.getSuggestedUsersbyID()) == 30
 
 def test_suggested_crawlers():
     api = TikTokApi()
     assert len(api.getSuggestedUsersbyIDCrawler(count=50)) == 50
-    assert len(api.getSuggestedHashtagsbyIDCrawler(count=50)) == 50
-    assert len(api.getSuggestedMusicIDCrawler(count=50)) == 50
+    assert len(api.getSuggestedHashtagsbyIDCrawler(count=50)) > 0
+    assert len(api.getSuggestedMusicIDCrawler(count=50)) > 0
