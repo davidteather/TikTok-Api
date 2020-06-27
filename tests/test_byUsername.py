ce92c7b5ce38088b32b08a73b436168c1f643f57
davidteather <davidteather@users.noreply.github.com>
2020-06-27 16:11:33 -0500

    Fixed Test

--- tests/test_byUsername.py
+++ tests/test_byUsername.py
@@ -2,6 +2,6 @@ from TikTokApi import TikTokApi
 
 def test_trending():
     api = TikTokApi()
-    assert abs(len(api.byHashtag('independent', 5))-5) <= 1
-    assert abs(len(api.byHashtag('independent', 10))-10) <= 1
-    assert abs(len(api.byHashtag('independent', 20))-20) <= 1
+    assert abs(len(api.byUsername('independent', 5))-5) <= 1
+    assert abs(len(api.byUsername('independent', 10))-10) <= 1
+    assert abs(len(api.byUsername('independent', 20))-20) <= 1
