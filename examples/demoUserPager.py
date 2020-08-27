7ecfc58fe9290ae55afa8d8b5c4e2fc59f787198
Michael D. Hoyle <hoylemd@gmail.com>
2020-08-27 10:20:06 -0400

    update user pager demo to be more manageable

--- examples/demoUserPager.py
+++ examples/demoUserPager.py
@@ -1,14 +1,17 @@
+from datetime import datetime
 from TikTokApi import TikTokApi
 
 api = TikTokApi(debug=True)
 
+
 def printPage(page):
     """Just prints out each post with timestamp and description"""
     for post in page:
-        print("{}: {}".format(post['createTime'], post['desc']))
+        print("{}: {}".format(datetime.fromtimestamp(post['createTime']), post['desc']))
+
 
 count = 20
-username = 'diply'
+username = 'therock'
 
 # count and list all of the posts for a given user with the pager
 total = 0
@@ -23,23 +26,24 @@ print('{} has {} posts'.format(username, total))
 all_posts = total
 
 # List all of the posts for a given user after a certain date
-AUG_19 = 1597889080000  # 2020-8-19 22:04:40 to be precise. Must be ms-precision UNIX timestamp
+
+APR_24 = 1587757438000  # 2020-04-24 15:43:58 to be precise. Must be ms-precision UNIX timestamp
 user = api.getUserObject(username)
-page = api.userPage(user['id'], user['secUid'], page_size=30, after=AUG_19)
+page = api.userPage(user['id'], user['secUid'], page_size=30, after=APR_24)
 
 printPage(page['items'])
 new_posts = len(page['items'])
-print('{} has {} posts after {}'.format(username, new_posts, AUG_19))
+print('{} has {} posts after {}'.format(username, new_posts, APR_24))
 
 
 # Count and list all of the posts before a certain date for a given user with the pager
 
 total = 0
-pager = api.getUserPager(username, page_size=count, before=AUG_19)
+pager = api.getUserPager(username, page_size=count, before=APR_24)
 
 for page in pager:
     printPage(page)
     total += len(page)
 
-print('{} has {} posts from before {}'.format(username, total, AUG_19))
+print('{} has {} posts from before {}'.format(username, total, APR_24))
 print('Should be {}'.format(all_posts - new_posts))
