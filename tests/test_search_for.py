d6d17e4487b41bb23a6db34c6dbc660f6f3ca8ae
David Teather <34144122+davidteather@users.noreply.github.com>
2020-09-12 17:11:29 -0500

    Added before & after

--- tests/test_search_for.py
+++ tests/test_search_for.py
@@ -2,6 +2,6 @@ from TikTokApi import TikTokApi
 
 def test_search_for():
     api = TikTokApi()
-    assert len(api.search_for_hashtags('a')) >= 0
-    assert len(api.search_for_music('a')) >= 0
-    assert len(api.search_for_users('a')) >= 0
+    assert len(api.search_for_hashtags('a')) == 28
+    assert len(api.search_for_music('a')) == 28
+    assert len(api.search_for_users('a')) == 28
