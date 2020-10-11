1bea0c4df68f75c0ad78d92da4ab141a035be57b
Ben West <Xodarap@users.noreply.github.com>
2020-10-11 09:14:57 -0700

    Use new hashtag API

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -1,7 +1,7 @@
 import random
 import requests
 import time
-from urllib.parse import urlencode
+from urllib.parse import urlencode, quote
 
 from .browser import browser
 
@@ -592,6 +592,24 @@ class TikTokApi:
         }
         api_url = "{}api/challenge/detail/?{}&{}".format(
             BASE_URL, self.__add_new_params__(), urlencode(query)
+        )
+        b = browser(api_url, proxy=proxy, executablePath=self.executablePath)
+        return self.getData(b, proxy=proxy)
+
+    def getHashtagDetails(self, hashtag, **kwargs) -> dict:
+        """Returns a hashtag object.
+
+          :param hashtag: The hashtag to search by.
+          :param language: The 2 letter code of the language to return.
+                           Note: Doesn't seem to have an affect.
+          :param proxy: The IP address of a proxy to make requests from.
+        """
+        region, language, proxy, minCursor, maxCursor, maxCount = self.__process_kwargs__(kwargs)
+        query = {
+            'language': language
+        }
+        api_url = "{}node/share/tag/{}?{}&{}".format(
+            BASE_URL, quote(hashtag), self.__add_new_params__(), urlencode(query)
         )
         b = browser(api_url, proxy=proxy, executablePath=self.executablePath)
         return self.getData(b, proxy=proxy)
---
+++ tests/test_getHashtagDetails.py
@@ -0,0 +1,12 @@
+from TikTokApi import TikTokApi
+
+def test_getHashtagDetails():
+    api = TikTokApi()
+    result = api.getHashtagDetails('programming')
+    assert result['body']['challengeData']['posts'] > 1000
+    assert result['body']['challengeData']['challengeId'] == "5878100"
+
+def test_non_latin1():
+    api = TikTokApi()
+    result = api.getHashtagDetails('селфи')
+    assert result['body']['challengeData']['posts'] > 1000
