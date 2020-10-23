b8a37958c342c3bc6f5462cff6643107920f7aae
David Teather <34144122+davidteather@users.noreply.github.com>
2020-10-23 14:15:59 -0500

    V3.5.7 Fixed For TikTok Endpoint Updates

--- tests/test_getHashtagDetails.py
+++ tests/test_getHashtagDetails.py
@@ -4,11 +4,11 @@ from TikTokApi import TikTokApi
 def test_getHashtagDetails():
     api = TikTokApi()
     result = api.getHashtagDetails("programming")
-    assert result["body"]["challengeData"]["posts"] > 1000
-    assert result["body"]["challengeData"]["challengeId"] == "5878100"
+    assert result["challengeInfo"]["stats"]['viewCount'] > 1000
+    assert result["challengeInfo"]['challenge']["id"] == "5878100"
 
 
 def test_non_latin1():
     api = TikTokApi()
     result = api.getHashtagDetails("селфи")
-    assert result["body"]["challengeData"]["posts"] > 1000
+    assert result["challengeInfo"]["stats"]['viewCount'] > 1000
