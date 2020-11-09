0c3ea7ecda6fe7f93c62bf9788f187ab721f3b8c
David Teather <34144122+davidteather@users.noreply.github.com>
2020-11-09 16:13:10 -0600

    Remove useragent assignment

--- TikTokApi/browser.py
+++ TikTokApi/browser.py
@@ -37,8 +37,6 @@ class browser:
         args = kwargs.get("browser_args", [])
         options = kwargs.get("browser_options", {})
 
-        self.userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
-
         if len(args) == 0:
             self.args = []
         else:
@@ -49,7 +47,6 @@ class browser:
             "handleSIGINT": True,
             "handleSIGTERM": True,
             "handleSIGHUP": True,
-            "userAgent": self.userAgent
         }
 
         if self.proxy != None:
