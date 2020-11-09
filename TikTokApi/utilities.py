c2bf51f1cc629d983ceccd6c7b4cffe1e1390d88
David Teather <34144122+davidteather@users.noreply.github.com>
2020-11-08 19:29:55 -0600

    Update utilities

--- TikTokApi/utilities.py
+++ TikTokApi/utilities.py
@@ -1,15 +1,17 @@
 import subprocess
 import sys
+import pkg_resources
 
 def update_messager():
-    if not check("TikTokApi"):
+    #if not check("TikTokApi"):
         # Outdated
-        print("TikTokApi package is outdated, please consider upgrading! \n(You can suppress this by setting ignore_version to True while calling the TikTok Api class)")
+    #    print("TikTokApi package is outdated, please consider upgrading! \n(You can suppress this by setting ignore_version to True while calling the TikTok Api class)")
 
     if not check_future_deprecation():
         print("Your version of python is going to be deprecated, for future updates upgrade to 3.7+")
 
 def check(name):
+    
     latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(name)], capture_output=True, text=True))
     latest_version = latest_version[latest_version.find('(from versions:')+15:]
     latest_version = latest_version[:latest_version.find(')')]
