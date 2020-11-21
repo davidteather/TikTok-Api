4339ffa89292fcc30577f6f29c695be8775787cd
David Teather <34144122+davidteather@users.noreply.github.com>
2020-11-20 22:22:02 -0600

    Update exceptions

--- TikTokApi/exceptions.py
+++ TikTokApi/exceptions.py
@@ -16,10 +16,10 @@ class TikTokNotFoundError(Exception):
 class EmptyResponseError(Exception):
     def __init__(self, message="TikTok sent no data back"):
         self.message = message
-        super().__init(self.message)
+        super().__init__(self.message)
 
 
 class JSONDecodeFailure(Exception):
     def __init__(self, message="TikTok sent invalid JSON back"):
         self.message = message
-        super().__init(self.message)
+        super().__init__(self.message)
