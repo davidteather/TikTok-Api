2bcc784c9ba92af41e88db87dc7445cf0bf9ca05
Vincent Demay <vincent@demay-fr.net>
2021-12-03 09:31:41 +0100

    Fix : tiktok.com seems to be blocked by CloudFront use www.tiktok.com instead (not blocked)

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -1289,7 +1289,7 @@ class TikTokApi:
             device_id,
         ) = self.__process_kwargs__(kwargs)
         r = requests.get(
-            "https://tiktok.com/@{}?lang=en".format(quote(username)),
+            "https://www.tiktok.com/@{}?lang=en".format(quote(username)),
             headers={
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 "authority": "www.tiktok.com",
@@ -1647,7 +1647,7 @@ class TikTokApi:
         * username: The username to get the secUid for
         """
         r = requests.get(
-            "https://tiktok.com/@{}?lang=en".format(quote(username)),
+            "https://www.tiktok.com/@{}?lang=en".format(quote(username)),
             headers={
                 "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
                 "authority": "www.tiktok.com",
