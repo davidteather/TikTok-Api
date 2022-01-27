df4eb23455aa607f489d6f97cf13a2aa023515d1
David Teather <34144122+davidteather@users.noreply.github.com>
2022-01-27 16:50:51 -0600

    Investigated discover_type, still doing work on it though

--- tests/test_search.py
+++ tests/test_search.py
@@ -6,19 +6,43 @@ api = TikTokApi(custom_verify_fp=os.environ.get("verifyFp", None))
 
 def test_discover_type():
     count = 0
+    found_ids = []
     for result in api.search.users("therock", count=50):
+        if result.user_id in found_ids:
+            raise Exception("Multiple Users on search.user")
+        found_ids.append(result.user_id)
         count += 1
 
     assert count >= 50
 
     count = 0
+    found_ids
     for result in api.search.sounds("funny", count=50):
+        if result.id in found_ids:
+            raise Exception("Multiple Sounds on search.sound")
+        found_ids.append(result.id)
         count += 1
 
     assert count >= 50
 
     count = 0
+    found_ids = []
     for result in api.search.hashtags("funny", count=50):
+        if result.id in found_ids:
+            raise Exception("Multiple Users on search.user")
+        found_ids.append(result.id)
+        count += 1
+
+    assert count >= 50
+
+
+def test_video():
+    count = 0
+    found_ids = []
+    for video in api.search.videos("therock", count=50):
+        if video.id in found_ids:
+            raise Exception("Duplicate Video on search.videos")
+        found_ids.append(video.id)
         count += 1
 
     assert count >= 50
@@ -26,7 +50,11 @@ def test_discover_type():
 
 def test_users_alternate():
     count = 0
+    found_ids = []
     for user in api.search.users_alternate("therock", count=50):
+        if user.user_id in found_ids:
+            raise Exception("Multiple Users on search.user")
+        found_ids.append(user.user_id)
         count += 1
 
     assert count >= 50
