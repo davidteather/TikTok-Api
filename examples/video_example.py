2b0882e7aef5323e00fe54bdf1a5e18e7e47934f
Edmundo Meyer <80543721+e-meyer@users.noreply.github.com>
2023-11-27 22:09:26 -0300

    Update video_example.py class name (#1056)
    I was looking at the examples and noticed that the name of the function in video_example.py is the same as in user_example.py, which led me to believe it was a mistake. I just changed it for better understanding.

--- examples/video_example.py
+++ examples/video_example.py
@@ -7,7 +7,7 @@ ms_token = os.environ.get(
 )  # set your own ms_token, think it might need to have visited a profile
 
 
-async def user_example():
+async def get_video_example():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
         video = api.video(
@@ -23,4 +23,4 @@ async def user_example():
 
 
 if __name__ == "__main__":
-    asyncio.run(user_example())
+    asyncio.run(get_video_example())
