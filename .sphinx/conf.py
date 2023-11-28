d27ddaef682b264e6565b184e6f5aa8d36557f8f
davidteather <34144122+davidteather@users.noreply.github.com>
2023-11-27 19:40:28 -0600

    bump version

--- .sphinx/conf.py
+++ .sphinx/conf.py
@@ -16,7 +16,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "TikTokAPI"
 copyright = "2023, David Teather"
 author = "David Teather"
-release = "v6.1.1"
+release = "v6.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/main/usage/configuration.html#general-configuration
