805d2d56539a82d7de372fc933a316df37719e81
davidteather <davidteather@users.noreply.github.com>
2023-11-28 01:48:55 +0000

    deploy: 4f2c13ff272ce83e9deb1a11ff10ceb52b6dcff1

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,5 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v6.1.1',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'v6.2.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
