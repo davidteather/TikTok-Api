254114fff2cfbb898e539b5ebbd20d6f48556f29
Lukas <ltanton273@gmail.com>
2024-12-05 14:40:55 -0500

    feat: add playlist example

---
+++ examples/playlist_example.py
@@ -0,0 +1,26 @@
+from TikTokApi import TikTokApi
+import asyncio
+import os
+
+ms_token = os.environ.get(
+    "ms_token", None
+)  # set your own ms_token, think it might need to have visited a profile
+
+
+async def user_example():
+    async with TikTokApi() as api:
+        await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
+        user = api.user("mrbeast")
+        user_data = await user.info()
+        print(user_data)
+
+        async for playlist in user.playlists(count=3):
+            print(playlist)
+            print(playlist.as_dict)
+
+            async for video in playlist.videos():
+                print(video)
+                print(video.as_dict)
+
+if __name__ == "__main__":
+    asyncio.run(user_example())
