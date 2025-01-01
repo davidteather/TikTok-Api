2227f55191cb6aa7e9cc5ac629efa2eba92e5119
Lukas <ltanton273@gmail.com>
2025-01-01 23:35:07 +0100

    style: remove unnecessary code

--- examples/playlist_example.py
+++ examples/playlist_example.py
@@ -11,8 +11,6 @@ async def user_example():
     async with TikTokApi() as api:
         await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3)
         user = api.user("mrbeast")
-        user_data = await user.info()
-        print(user_data)
 
         async for playlist in user.playlists(count=3):
             print(playlist)
