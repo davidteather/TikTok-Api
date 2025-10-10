47a42c7ef6061060d63489ffa3847545a6da2c4a
Kevin Burke <k_burke09@hotmail.com>
2025-10-10 16:09:17 +0100

    Log context cookies

--- TikTokApi/tiktok.py
+++ TikTokApi/tiktok.py
@@ -479,7 +479,7 @@ class TikTokApi:
                     self.logger.debug(
                         f"→ Request: {request.method} {request.url[:100]} "
                         f"[{request.resource_type}]"
-                        f"[Request headers: {request_headers}]"
+                        f"[Request headers: {request_headers}. Cookies added to context: {formatted_cookies}]"
                     )
 
                 def log_response(response):
